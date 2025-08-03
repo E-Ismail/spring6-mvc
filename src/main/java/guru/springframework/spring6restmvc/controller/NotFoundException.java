05475079018c1b011cf1d2735914f2509305f13b
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2025-08-04 01:22:42 +0200

    Added ResponseStatus annotation and removed ExceptionController.java

--- src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
@@ -6,6 +6,10 @@ package guru.springframework.spring6restmvc.controller;
  * @create 04/08/2025 - 00:51
  */
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
