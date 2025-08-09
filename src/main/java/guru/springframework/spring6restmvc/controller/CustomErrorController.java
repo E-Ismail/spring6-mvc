4a403a46dd2bdab91c54c4acfa24e7f26558ab51
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2025-08-10 01:02:07 +0200

    handleJPAViolation TransactionSystemException

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
@@ -6,16 +6,35 @@ package guru.springframework.spring6restmvc.controller;
  * @create 09/08/2025 - 22:56
  */
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity<?> handleJPAViolations(TransactionSystemException transactionSystemException) {
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+        if (transactionSystemException.getCause().getCause() instanceof ConstraintViolationException) {
+            ConstraintViolationException ve = (ConstraintViolationException) transactionSystemException.getCause().getCause();
+            List<Map<String,String>> errors = ve.getConstraintViolations().stream()
+                    .map(constraintViolation -> {
+                        Map<String, String> errMap = new HashMap<>();
+                        errMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+                        return errMap;
+                    }).toList();
+            return responseEntity.body(errors);
+        }
+        return responseEntity.build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity<?> handleBindErrors(MethodArgumentNotValidException exception) {
@@ -27,4 +46,5 @@ public class CustomErrorController {
                 }).toList();
         return ResponseEntity.badRequest().body(fieldErrors);
     }
+
 }
--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -1,23 +1,35 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.mappers.BeerMapper;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 /*
  * @author Ech-Cherrate Ismail
  * @project spring-6-rest-mvc
@@ -34,6 +46,37 @@ class BeerControllerIT {
     BeerRepository beerRepository;
     @Autowired
     private BeerMapper beerMapper;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        //Setting up mockMVC environment with the spring data repositories injected in the services
+        //A full spring boot test, bringing it all!
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void testPatchBeer() throws Exception {
+        Beer beer = beerRepository.findAll().get(0);
+
+        Map<String, Object> beerMap = new HashMap<>();
+        beerMap.put("beerName", "New Name very long very very very very very very very very very very very very very very very very very very LOOOOONNNNG");
+
+        MvcResult mvcResult = mockMvc.perform(patch(BeerController.BEER_PATH + "/" + beer.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerMap)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.length()", is(1)))
+                .andReturn();
+        System.err.println(mvcResult.getResponse().getContentAsString());
+    }
 
     @Test
     void testPatchByIdNotFound() {
@@ -51,7 +94,7 @@ class BeerControllerIT {
         BeerDTO beerDTO = beerMapper.beerToBeerDTO(beer);
         beerDTO.setId(null);
         beerDTO.setVersion(null);
-        final String beerName = beer.getBeerName()+ "PATCHED";
+        final String beerName = beer.getBeerName() + "PATCHED";
         beerDTO.setBeerName(beerName);
 
         ResponseEntity<HttpStatus> responseEntity = beerController.updateBeerPatchById(beer.getId(), beerDTO);
@@ -70,7 +113,7 @@ class BeerControllerIT {
     @Rollback
     @Transactional
     @Test
-    void testDeleteByIdFound() throws Exception{
+    void testDeleteByIdFound() throws Exception {
         Beer beer = beerRepository.findAll().get(0);
         ResponseEntity<HttpStatus> responseEntity = beerController.deleteBeerById(beer.getId());
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
