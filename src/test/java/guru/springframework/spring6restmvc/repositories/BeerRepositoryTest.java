e686ee6ceb308a5bd2bf1c863b63348a1715d842
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2025-08-10 00:31:00 +0200

    assertThrows ConstraintViolationException

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 /*
  * @author Ech-Cherrate Ismail
  * @project spring-6-rest-mvc
@@ -22,12 +24,26 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void testSaveBeerNameTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer beer = Beer.builder()
+                    .beerName("Very Long Name But that is returning a data DataIntegrityViolationException")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("12345")
+                    .price(BigDecimal.valueOf(12.34))
+                    .build();
+            beerRepository.save(beer);
+            beerRepository.flush();
+        });
+    }
+
+    @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                 .beerName("Badweiser")
-                        .beerStyle(BeerStyle.PALE_ALE)
-                        .upc("12345")
-                        .price(new BigDecimal("11.99"))
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12345")
+                .price(new BigDecimal("11.99"))
                 .build());
         //Running so quickly returning back beer object and not rushing to db
         //Sometimes hibernate will flush and batch, but the session is ending so quickly
