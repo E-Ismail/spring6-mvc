343117b80e4cd82f473d284d780de89e25018b35
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2025-08-16 22:37:25 +0200

    refactor

--- src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
@@ -26,7 +26,7 @@ public class BootstrapData implements CommandLineRunner {
     private final CustomerRepository customerRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         loadBeerData();
         loadCustomerData();
     }
