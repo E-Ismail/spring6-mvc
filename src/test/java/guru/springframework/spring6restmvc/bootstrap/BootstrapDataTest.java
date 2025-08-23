1b379ce3d1fa97ee8043894dccd38cb31d33d818
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2025-08-23 14:42:19 +0200

    save csvData to DB

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.bootstrap;
 
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import guru.springframework.spring6restmvc.services.BeerCsvService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +25,16 @@ class BootstrapDataTest {
     private CustomerRepository customerRepository;
 
     BootstrapData bootstrapData;
+    @Autowired
+    private BeerCsvService beerCsvService;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, beerCsvService);
     }
 
     @Test
-    void run() {
+    void run() throws Exception {
         bootstrapData.run();
         assertThat(beerRepository.count()).isEqualTo(3);
         assertThat(customerRepository.count()).isEqualTo(3);
